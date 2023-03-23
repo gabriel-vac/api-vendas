a536efd31a1834392a30ace36caf3df49b626f22
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2023-03-23 02:24:53 -0300

    feat: s3 provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,42 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tempFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
   directory: uploadFolder,
   tempFolder,
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${file.originalname}`;
 
-      callback(null, filename);
+        callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    aws: {
+      bucket: 'api-vendas-curso-node-solid',
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,6 +1,8 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
+import S3StorageProvider from '@shared/providers/StorageProvider/S3StorageProvider';
+import uploadConfig from '@config/upload';
 import User from '../typeorm/entities/User';
 import { UserRepository } from '../typeorm/repositories/UserRepository';
 
@@ -12,7 +14,6 @@ interface IRequest {
 class UpdateUserAvatarService {
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const userRepository = getCustomRepository(UserRepository);
-    const storageProvider = new DiskStorageProvider();
 
     const user = await userRepository.findById(user_id);
 
@@ -20,13 +21,21 @@ class UpdateUserAvatarService {
       throw new AppError('User not found');
     }
 
-    if (user.avatar) {
-      await storageProvider.deleteFile(user.avatar);
+    if (uploadConfig.driver === 's3') {
+      const s3Provider = new S3StorageProvider();
+      if (user.avatar) {
+        await s3Provider.deleteFile(user.avatar);
+      }
+      const filename = await s3Provider.saveFile(avatarFilename);
+      user.avatar = filename;
+    } else {
+      const diskProvider = new DiskStorageProvider();
+      if (user.avatar) {
+        await diskProvider.deleteFile(user.avatar);
+      }
+      const filename = await diskProvider.saveFile(avatarFilename);
+      user.avatar = filename;
     }
-
-    const filename = await storageProvider.saveFile(avatarFilename);
-
-    user.avatar = filename;
 
     await userRepository.save(user);
 
