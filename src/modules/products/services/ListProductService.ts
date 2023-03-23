72115fe05711bda54d932827af31d3b5c29e4b78
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2023-03-23 02:32:25 -0300

    refactor: RedisCache class performance refact

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,13 +1,11 @@
 import { getCustomRepository } from 'typeorm';
-import RedisCache from '@shared/cache/RedisCache';
+import redisCache from '@shared/cache/RedisCache';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productRepository = getCustomRepository(ProductRepository);
-
-    const redisCache = new RedisCache();
 
     let products = await redisCache.recover<Product[]>(
       'api-vendas-PRODUCT_LIST'
